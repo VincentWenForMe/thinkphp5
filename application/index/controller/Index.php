31ea40ec200ebd1ee583b977ef4aa33789850509
litian1218 <1181551049@qq.com>
2016-05-31 20:58:03 +0800

    测试不同类型方法在浏览器中的可访问性
    public protect private

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -7,4 +7,29 @@ class Index
     {
         return 'hello,' . $name;
     }
+
+    /**
+     * 测试不同类型方法在浏览器中的可访问性
+     * public protect private
+     */
+    // hello方法
+    public function hello()
+    {
+        return 'hello!';
+    }
+    // test方法
+    public function test()
+    {
+        return 'test!';
+    }
+    // hello2方法
+    protected function hello2()
+    {
+        return 'hello2';
+    }
+    // test2方法
+    private function test2()
+    {
+        return 'test2';
+    }
 }
