d8176786ebc82b19eb45ea0f16e561efa31dbcd2
litian1218 <1181551049@qq.com>
2016-06-15 17:13:12 +0800

    解决路由问题
    配置了vhost 解决了路由问题 顺便研究了一下thinkphp的路由

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -8,16 +8,28 @@ use app\index\model\User as UserModel; //引用user类
 /**
 *
 */
-class User
+class User extends Controller
 {
+    public function index()
+    {
+        //对象实例化
+        $User  = new UserModel();
+        //取数据
+        $users = $User::all();
+        // dump($users);
+
+        // 数据传入V层
+        $this->assign('users',$users);
+        return $this->fetch();
+    }
     // 批量新增
     public function addList()
     {
         $User = new UserModel();
         $list = [
-            ['nickname'=>'meiko','email'=>'123@11.com','birthday'=>strtotime('1999-06-01')],
-            ['nickname'=>'deft','email'=>'132@11.com','birthday'=>strtotime('1999-05-02')],
-            ['nickname'=>'marin','email'=>'123@11.com','birthday'=>strtotime('199706701')],
+            ['nickname'=>'meiko1','email'=>'123@11.com','birthday'=>strtotime('1999-06-01')],
+            // ['nickname'=>'deft','email'=>'132@11.com','birthday'=>strtotime('1999-05-02')],
+            // ['nickname'=>'marin','email'=>'123@11.com','birthday'=>strtotime('199706701')],
         ];
         if ($User->saveAll($list)) {
             return '用户批量新增成功';
--- application/route.php
+++ application/route.php
@@ -15,10 +15,13 @@ return [
     '__pattern__'         => [
         'id'    => '\d+',
     ],
+    // 添加路由规则 路由到 index控制器的add操作方法
+    'add/[:name]' => 'index/add',
+
+    'user/add_list'   => 'index/user/addList',
     'user/index'      => 'index/user/index',
     'user/create'     => 'index/user/create',
     'user/add'        => 'index/user/add',
-    'user/add_list'   => 'index/user/addList',
     'user/update/:id' => 'index/user/update',
     'user/delete/:id' => 'index/user/delete',
     'user/:id'        => 'index/user/read',
