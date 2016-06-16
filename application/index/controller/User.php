d41da758bb0658fc889d032c86cc26c23fcc438c
litian1218 <1181551049@qq.com>
2016-06-16 10:40:20 +0800

    read方法

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -27,9 +27,9 @@ class User extends Controller
     {
         $User = new UserModel();
         $list = [
-            ['nickname'=>'meiko','email'=>'123@11.com','birthday'=>strtotime('19990601')],
-            // ['nickname'=>'deft','email'=>'132@11.com','birthday'=>strtotime('1999-05-02')],
-            // ['nickname'=>'marin','email'=>'123@11.com','birthday'=>strtotime('19970701')],
+            ['nickname'=>'Meiko','email'=>'123@11.com','birthday'=>strtotime('19990601')],
+            ['nickname'=>'Deft','email'=>'132@11.com','birthday'=>strtotime('1999-05-02')],
+            ['nickname'=>'Pawn','email'=>'1234@11.com','birthday'=>strtotime('19970701')],
         ];
         if ($User->saveAll($list)) {
             return '用户批量新增成功';
@@ -38,4 +38,14 @@ class User extends Controller
             return $User->getError();
         }
     }
+
+    public function read($id='')
+    {
+        $user = UserModel::get($id);
+        // dump($user);
+        echo $user->nickname . '<br/>';
+        echo $user->email . '<br/>';
+        echo date('Y/m/d', $user->birthday) . '<br/>';
+    }
+
 }
